bc0f932a2071c4e7ebe10520efa2ff15f5239d6b
Lucas Santos <lucassm02@gmail.com>
2020-03-05 01:09:22 -0300

    GoStack react app

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <View>
+                <Text style={styles.text}>OPa caraaaaai hhshshshhs</Text>
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 50,
+        color: 'red',
+    },
+});
+
+export default App;
